1cc879cb3a67c80a6c160d5fa5ccf846f659e904
Axel Karjalainen <axel@axka.fi>
2026-05-22 12:19:23 +0300

    Show dessert
    - Show every meal package, with non-vegetarian lunch first like previously
    - Format everything with Prettier
    - Rename JavaScript identifiers to camelCase, which is standard
    - Add TypeScript types using JSDoc annotations

--- javascript.js
+++ javascript.js
@@ -21,12 +21,6 @@ var select = document.getElementById('dd');
 
 
 
-
-let menu = []
-
-
-
-
 let can_continue = false
 
 
@@ -79,7 +73,6 @@ function changeDay(){
 
     document.getElementById('dayLbl').innerHTML=fin_days[days.indexOf(today)];
     loadCourse()
-    displayMenuData()
 }
 
 
@@ -102,8 +95,6 @@ function Init() {
     document.getElementById('dayLbl').innerHTML=fin_days[days.indexOf(today)];
     document.getElementById('resultLbl').innerHTML="?";
     loadCourse()
-    menu = localStorage.getItem("menu")
-    
 }
 window.onload = Init;
 
@@ -213,55 +204,3 @@ function loadCourse()
     setMyCourse()
     showResult()
 }
-
-
-
-
-
-
-
-
-//HANDLING MENU
-function getMenuData(){
-
-    //GETTING MENU DATA
-    fetch('https://ksyk-menu-scraper.onrender.com/').then(function (response) 
-    {
-        // The API call was successful!
-        return response.text();
-    }).then(function (html) {
-
-        menu = html
-        localStorage.setItem("menu", html);
-        displayMenuData()
-        
-
-
-    }).catch(function (err) {
-        // There was an error
-        console.warn('Something went wrong1.', err);
-    });
-
-}
-
-
-function displayMenuData()
-{
-    
-    document.getElementById("menu_list").innerHTML = ""
-
-    var a = JSON.parse(menu)
-    var menu_arr = a["menu-data"][today].split(";");
-
-
-    document.getElementById("menu_week").innerHTML = a["menu-data"]["Week"]
-
-
-    for (i in menu_arr)
-    {
-        document.getElementById("menu_list").innerHTML += menu_arr[i] + "<br />"
-    }
-
-}
-
-
--- menuscript.js
+++ menuscript.js
@@ -1,4 +1,10 @@
-const day_list = [
+// Define API types for TypeScript using JSDoc annotations: https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html
+/** @typedef {Record<string, { amount: number, unit: string }>} Macros */
+/** @typedef {{ diets: string, ingredients: string, macros: Macros | null }} Meal */
+/** @typedef {{ [packageName: string]: [mealName: string, Meal | null][] }} MealPackage */
+/** @typedef {{ Info: Record<string, string>, Menu: Record<string, MealPackage[]>, Week: number }} MenuApiData */
+
+const dayList = [
   "Sunday",
   "Monday",
   "Tuesday",
@@ -7,7 +13,7 @@ const day_list = [
   "Friday",
   "Saturday",
 ];
-const day_list_finnish = [
+const dayListFinnish = [
   "Sunnuntai",
   "Maanantai",
   "Tiistai",
@@ -18,49 +24,110 @@ const day_list_finnish = [
 ];
 let day = "Wednesday";
 
-let public_data = "";
+let publicData = "";
 
 document.addEventListener(
   "DOMContentLoaded",
   function () {
     const date = new Date();
-    let day_of_week = date.getDay();
+    let dayOfWeek = date.getDay();
 
     // Saturday and Sunday to Friday
-    if (day_of_week == 6 || day_of_week == 0) {
-      day_of_week = 5;
+    if (dayOfWeek == 6 || dayOfWeek == 0) {
+      dayOfWeek = 5;
     }
 
-    day = day_list[day_of_week];
+    day = dayList[dayOfWeek];
 
-    document.getElementById("day_label").innerHTML =
-      day_list_finnish[day_list.indexOf(day)];
+    document.getElementById("day_label").innerText =
+      dayListFinnish[dayList.indexOf(day)];
 
     //on start set public data from memory, the load new data from website to it
-    public_data = JSON.parse(localStorage.getItem("menu_data"));
-    
-    get_menu_data();
+    publicData = JSON.parse(localStorage.getItem("menu_data"));
+
+    getMenuData();
   },
-  false
+  false,
 );
 
-// type "lounaat" or "kasvislounaat"
-function create_food_element(name, diet, macro_element, type, ingredients) {
-  const element_id = Math.floor(Math.random() * 90000000);
+function renderDay() {
+  document.getElementById("week_count").innerText = `Viikko ${retrieveWeek()}`;
+
+  /** @type {MenuApiData} */
+  const parsedData = JSON.parse(publicData);
+  const menu = parsedData["Menu"];
+  const packages = menu[day].flatMap((val) => Object.entries(val));
+
+  // Swap the first two packages (e.g. kasvislounas and lounas)
+  if (packages.length > 1) {
+    [packages[0], packages[1]] = [packages[1], packages[0]];
+  }
+
+  const packagesElem = document.getElementById("meal_packages");
+  packagesElem.innerText = ""; // Empty the menu
+  packages.forEach(([packageName, meals]) => {
+    renderMealPackage(packageName, meals, packagesElem);
+  });
+}
+
+/**
+ * Renders a meal package.
+ * @param {string} packageName
+ * @param {[mealName: string, Meal | null][]} meals
+ * @param {Element} parentElem
+ */
+function renderMealPackage(packageName, meals, parentElem) {
+  const packageElem = document.createElement("div");
+
+  const labelElem = document.createElement("h3");
+  labelElem.innerText = packageName;
+  packageElem.appendChild(labelElem);
+  packageElem.appendChild(document.createElement("hr"));
+
+  const mealsElem = document.createElement("div");
+  mealsElem.classList.add("accordion", "accordion-flush");
+
+  meals.forEach(([mealName, meal]) => {
+    console.log("Meal:", mealName, meal);
+    if (meal != null) {
+      renderMeal(
+        mealName,
+        meal.diets,
+        renderMacros(meal.macros),
+        mealsElem,
+        meal.ingredients,
+      );
+    } else {
+      renderMeal(mealName, "", "", mealsElem, "ei saatavilla");
+    }
+  });
+
+  packageElem.appendChild(mealsElem);
+  parentElem.appendChild(packageElem);
+}
+
+/**
+ * Renders a meal.
+ * @param {string} name - Name of the meal.
+ * @param {string} diet - Dietary labels.
+ * @param {string} macroHtml - HTML string of the macronutrient information from {@link renderMacros}.
+ * @param {Element} parentElem - Parent element.
+ * @param {string} ingredients - Ingredient information.
+ */
+function renderMeal(name, diet, macroHtml, parentElem, ingredients) {
+  const elementId = Math.floor(Math.random() * 90000000).toString();
 
   const container = document.createElement("div");
-  container.setAttribute("class", "accordion-item");
-  const parent = document.getElementById(type);
-  parent.appendChild(container);
+  container.classList.add("accordion-item");
 
-  const accordion_header = `<h2 class="accordion-header">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${element_id}" aria-expanded="false" aria-controls="flush-collapseOne">
+  const accordionHeader = `<h2 class="accordion-header">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${elementId}" aria-expanded="false" aria-controls="flush-collapseOne">
     ${name}${diet == "" ? "" : ` (${diet})`}
     </button>
   </h2>`;
 
-  const macro_element_wrapper =
-    macro_element == ""
+  const macroElementWrapper =
+    macroHtml == ""
       ? ""
       : `<div class="card fs-6">
     <div class="card-header text-start container bg-body-secondary">
@@ -102,14 +169,18 @@ function create_food_element(name, diet, macro_element, type, ingredients) {
             Kuitu
           </p>
         </div>
-        ${macro_element}
+        ${macroHtml}
       </div>
     </div>
   </div>`;
 
-  const accordion_body = `<div id="${element_id}" class="accordion-collapse collapse" data-bs-parent="#${type}">
+  // Give the parent element an ID
+  if (parentElem.id == "")
+    parentElem.id = Math.floor(Math.random() * 90000000).toString();
+
+  const accordionBody = `<div id="${elementId}" class="accordion-collapse collapse" data-bs-parent="#${parentElem.id}">
     <div class="accordion-body" style="padding-left: 0px; padding-right: 0px;">
-      ${macro_element_wrapper}
+      ${macroElementWrapper}
       <div class="card fs-6" style="margin-top: 5px;">
         <div class="card-header text-start container bg-body-secondary">
           <div class="col"><strong>Ainesosat</strong></div>
@@ -126,148 +197,64 @@ function create_food_element(name, diet, macro_element, type, ingredients) {
     </div>
   </div>`;
 
-  container.innerHTML = accordion_header + accordion_body;
-}
-
-function render_day() {
-  document.getElementById("week_count").innerHTML = `Viikko ${retrieve_week()}`;
-
-  //empty the menu
-  document.getElementById("lounaat").innerHTML = "";
-  document.getElementById("kasvislounaat").innerHTML = "";
-
-  parsed_data = JSON.parse(public_data);
-  var menu = parsed_data["Menu"];
-
-  day_menu = menu[day];
-
-  lounas_options = 0;
-  //element 1 is the lounas
-  //console.log(Object.keys(day_menu[1]))
-  console.log(Object.keys(day_menu[0]));
-  try {
-    lounas_options = day_menu[1][Object.keys(day_menu[1])[0]];
-  } catch {}
-  kasvis_options = day_menu[0][Object.keys(day_menu[0])[0]];
-
-  //rendering the lounas options
-  if (lounas_options != 0) {
-    document.getElementById("lounas_lbl").innerHTML = Object.keys(
-      day_menu[1]
-    )[0];
-
-    for (var i = 0; i < lounas_options.length; i++) {
-      lounas_name = lounas_options[i][0];
-      lounas_diet = "";
-      console.log(lounas_options[i]);
-      if (lounas_options[i][1] != null) {
-        lounas_diet = lounas_options[i][1]["diets"];
-        create_food_element(
-          lounas_name,
-          lounas_diet,
-          render_macros(lounas_options[i][1]["macros"]),
-          "lounaat",
-          lounas_options[i][1]["ingredients"]
-        );
-      } else {
-        create_food_element(lounas_name, lounas_diet, "", "lounaat", "ei saatavilla");
-      }
-    }
-  }
-
-  //rendering the kasvis lounas options
-  document.getElementById("kasvislounas_lbl").innerHTML = Object.keys(
-    day_menu[0]
-  )[0];
-
-  for (var i = 0; i < kasvis_options.length; i++) {
-    kasvislounas_name = kasvis_options[i][0];
-
-    kasvislounas_diet = "";
-    console.log(kasvis_options[i]);
-    if (kasvis_options[i][1] != null) {
-      kasvislounas_diet = kasvis_options[i][1]["diets"];
-      create_food_element(
-        kasvislounas_name,
-        kasvislounas_diet,
-        render_macros(kasvis_options[i][1]["macros"]),
-        "kasvislounaat",
-        kasvis_options[i][1]["ingredients"]
-      );
-    } else {
-      create_food_element(
-        kasvislounas_name,
-        kasvislounas_diet,
-        "",
-        "kasvislounaat",
-        "ei saatavilla",
-      );
-    }
-  }
+  container.innerHTML = accordionHeader + accordionBody;
+  parentElem.appendChild(container);
 }
 
 /**
- * Renders macronutrients
- *
- * @param {Object[]} macro_list - Map of macronutrients
+ * Renders macronutrients.
+ * @param {Macros | null} macros - Map of macronutrients.
  * @returns {string}
  */
-function render_macros(macro_list) {
-  console.log(macro_list);
-  if (macro_list == null) {
+function renderMacros(macros) {
+  const renderMacro = (name) => {
+    try {
+      return `${macros[name]["amount"]} ${macros[name]["unit"]}`;
+    } catch {
+      return "ei tiedossa";
+    }
+  };
+
+  console.log("Macros:", macros);
+  if (macros == null) {
     return "";
   }
 
   const output = `<div class="col text-end" style="font-weight: 200;">
     <p class="card-text ruoka-kortti">
-    ${render_macro("EnergyKj", macro_list)}, ${render_macro(
-    "EnergyKcal",
-    macro_list
-  )}
+    ${renderMacro("EnergyKj")}, ${renderMacro("EnergyKcal")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("Fat", macro_list)}
+    ${renderMacro("Fat")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("FatSaturated", macro_list)}
+    ${renderMacro("FatSaturated")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("Carbohydrates", macro_list)}
+    ${renderMacro("Carbohydrates")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("Sugar", macro_list)}
+    ${renderMacro("Sugar")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("Protein", macro_list)}
+    ${renderMacro("Protein")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("Salt", macro_list)}
+    ${renderMacro("Salt")}
     </p>
     <p class="card-text ruoka-kortti">
-    ${render_macro("Fiber", macro_list)}
+    ${renderMacro("Fiber")}
     </p>
   </div>`;
 
   return output;
 }
 
-function render_macro(name, macro_list) {
-  try {
-    return `${macro_list[name]["amount"]} ${macro_list[name]["unit"]}`;
-  } catch {
-    return "ei tiedossa";
-  }
-}
-
-function get_menu_data() {
+function getMenuData() {
   fetch("https://ksyk-menu-scraper.onrender.com/")
-    .then(function (response) {
-      // The API call was successful!
-      return response.text();
-    })
+    .then(resp => resp.text())
     .then(function (output) {
-      console.log(output);
-      public_data = output;
+      publicData = output;
       localStorage.setItem("menu_data", output);
     })
     .catch(function (err) {
@@ -276,10 +263,11 @@ function get_menu_data() {
     });
 }
 
-function retrieve_week() {
-  parsed_data = JSON.parse(public_data);
-  var week = parsed_data["Week"];
-  console.log(week);
+function retrieveWeek() {
+  /** @type {MenuApiData} */
+  const parsedData = JSON.parse(publicData);
+  var week = parsedData["Week"];
+  console.log("Current week:", week);
   return week;
 }
 
@@ -287,30 +275,30 @@ function test() {
   alert("bruh");
 }
 
-function open_next_day() {
-  if (day_list.indexOf(day) < 5) {
-    day = day_list[day_list.indexOf(day) + 1];
-    document.getElementById("day_label").innerHTML =
-      day_list_finnish[day_list.indexOf(day)];
-    render_day();
+function openNextDay() {
+  if (dayList.indexOf(day) < 5) {
+    day = dayList[dayList.indexOf(day) + 1];
+    document.getElementById("day_label").innerText =
+      dayListFinnish[dayList.indexOf(day)];
+    renderDay();
   } else {
-    day = day_list[1];
-    document.getElementById("day_label").innerHTML =
-      day_list_finnish[day_list.indexOf(day)];
-    render_day();
+    day = dayList[1];
+    document.getElementById("day_label").innerText =
+      dayListFinnish[dayList.indexOf(day)];
+    renderDay();
   }
 }
 
-function open_previous_day() {
-  if (day_list.indexOf(day) > 1) {
-    day = day_list[day_list.indexOf(day) - 1];
-    document.getElementById("day_label").innerHTML =
-      day_list_finnish[day_list.indexOf(day)];
-    render_day();
+function openPreviousDay() {
+  if (dayList.indexOf(day) > 1) {
+    day = dayList[dayList.indexOf(day) - 1];
+    document.getElementById("day_label").innerText =
+      dayListFinnish[dayList.indexOf(day)];
+    renderDay();
   } else {
-    day = day_list[5];
-    document.getElementById("day_label").innerHTML =
-      day_list_finnish[day_list.indexOf(day)];
-    render_day();
+    day = dayList[5];
+    document.getElementById("day_label").innerText =
+      dayListFinnish[dayList.indexOf(day)];
+    renderDay();
   }
 }
